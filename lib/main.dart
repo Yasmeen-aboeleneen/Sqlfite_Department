950f3fe1de876beb0f096da2b06320fd81b5a0af
Yasmeen-aboeleneen <yasmeenaboeleneen3@gmail.com>
2024-07-14 08:07:34 +0300

    Open dataBase

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart' as SqfliteDataBase;
+import 'package:sqflite/sqlite_api.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  String databasesPath = await SqfliteDataBase.getDatabasesPath();
+  print(databasesPath); // /data/user/0/com.example.sqflite_department/databases
+  String myDbName = "testDatabase.db";
+  String myDbPath = "$databasesPath/$myDbName";
+  int versionDb = 1;
+  SqfliteDataBase.openDatabase(
+    myDbPath,
+    version: versionDb,
+    onCreate: (Database db, int version) {
+      print(db);
+      print(version);
+    },
+  );
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        
-      ),
-       
+      theme: ThemeData(),
     );
   }
 }
-
- 
